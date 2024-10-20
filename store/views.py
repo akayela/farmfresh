5d9f04a44e45bf0fcbdc306235798c656a402da9
ak <akayela>
2024-10-20 16:11:48 +0200

    search working

--- store/views.py
+++ store/views.py
@@ -5,7 +5,9 @@ from carts.models import CartItem
 from carts.views import _cart_id
 
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.db.models import OrderBy
+from django.db.models import Q
+
+from django.http import HttpResponse
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -46,4 +48,32 @@ def product_detail(request,category_slug, product_slug):
     }
 
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('created_date').filter(Q(product_description__icontains=keyword) | Q(product_name__icontains=keyword)) # if your keyword is in the product_description
+            product_count    = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
